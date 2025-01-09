4713aa4a6b6f104a433503979e2b5ce95022dc9c
phortheman <marcuspullo@gmail.com>
2025-01-09 00:01:38 -0500

    feat(cs): refactored for new project structure

---
+++ cs/2021/01/main.cs
@@ -0,0 +1,108 @@
+﻿
+using System.IO;
+
+namespace Solver
+{
+
+    public class Solver
+    {
+        public static List<int> GenerateInput(String filePath)
+        {
+            List<int> output = new();
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"The file at {filePath} was not found.");
+            }
+
+            using (StreamReader reader = new(filePath))
+            {
+                String? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    output.Add(int.Parse(line));
+                }
+            }
+
+            return output;
+        }
+
+        public static int GetNumberOfIncreases(List<int> input, int lastIndex = 0)
+        {
+            if (lastIndex == 0)
+            {
+                lastIndex = input.Count;
+            }
+            int numOfIncreases = 0;
+            for (int i = 1; i < lastIndex; i++)
+            {
+                if (input[i - 1] < input[i])
+                {
+                    numOfIncreases++;
+                }
+            }
+            return numOfIncreases;
+        }
+
+        public static List<int> GetWindow(List<int> input)
+        {
+            List<int> window = new();
+
+            for (int i = 0; i < input.Count - 2; i++)
+            {
+                window.Add(input[i] + input[i + 1] + input[i + 2]);
+            }
+
+            return window;
+        }
+
+        private static string GetInputFilePath(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-i" && i + 1 < args.Length)
+                {
+                    string inputPath = args[i + 1];
+                    if (File.Exists(inputPath))
+                    {
+                        return inputPath;
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException($"The specified file does not exist: {inputPath}");
+                    }
+                }
+            }
+
+            string defaultPath = Path.Combine("..", "..", "..", "inputs", "2021", "01", "input.txt");
+
+            if (!File.Exists(defaultPath))
+            {
+                throw new FileNotFoundException($"The default input file was not found at {defaultPath}");
+            }
+
+            return defaultPath;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string inputFilePath = GetInputFilePath(args);
+                List<int> inputData = GenerateInput(inputFilePath);
+
+                int partOne = GetNumberOfIncreases(inputData);
+                int partTwo = GetNumberOfIncreases(GetWindow(inputData));
+
+                Console.WriteLine($"Part One: {partOne}");
+                Console.WriteLine($"Part Two: {partTwo}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+    }
+
+}
---
+++ cs/2021/02/main.cs
@@ -0,0 +1,146 @@
+﻿
+using System.IO;
+
+namespace Solver
+{
+
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        FORWARD
+    }
+
+    public struct Instruction
+    {
+        public Direction direction;
+        public int units;
+    }
+
+    public struct Position
+    {
+        public int horizontal;
+        public int depth;
+        public int aim;
+
+        public void RunInstruction(Instruction instruction)
+        {
+            switch (instruction.direction)
+            {
+                case Direction.UP:
+                    {
+                        depth -= instruction.units;
+                        break;
+                    }
+                case Direction.DOWN:
+                    {
+                        depth += instruction.units;
+                        break;
+                    }
+                case Direction.FORWARD:
+                    {
+                        horizontal += instruction.units;
+                        break;
+                    }
+            }
+        }
+
+        public void RunWithAim(Instruction instruction)
+        {
+            switch (instruction.direction)
+            {
+                case Direction.UP:
+                    {
+                        aim -= instruction.units;
+                        break;
+                    }
+                case Direction.DOWN:
+                    {
+                        aim += instruction.units;
+                        break;
+                    }
+                case Direction.FORWARD:
+                    {
+                        horizontal += instruction.units;
+                        depth += aim * instruction.units;
+                        break;
+                    }
+            }
+        }
+    }
+
+    public class Solver
+    {
+        public Solver() { }
+
+        public static List<Instruction> GenerateInput(String filePath)
+        {
+            List<Instruction> output = new();
+
+            using (StreamReader reader = new(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Instruction instruction = new();
+                    string[] fields = line.Split(" ");
+
+                    Enum.TryParse(fields[0].ToUpper(), out instruction.direction);
+                    int.TryParse(fields[1], out instruction.units);
+
+                    output.Add(instruction);
+                }
+            }
+
+            return output;
+        }
+
+        private static string GetInputFilePath(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-i" && i + 1 < args.Length)
+                {
+                    string inputPath = args[i + 1];
+                    if (File.Exists(inputPath))
+                    {
+                        return inputPath;
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException($"The specified file does not exist: {inputPath}");
+                    }
+                }
+            }
+
+            string defaultPath = Path.Combine("..", "..", "..", "inputs", "2021", "02", "input.txt");
+
+            if (!File.Exists(defaultPath))
+            {
+                throw new FileNotFoundException($"The default input file was not found at {defaultPath}");
+            }
+
+            return defaultPath;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputFilePath = GetInputFilePath(args);
+            List<Instruction> inputData = GenerateInput(inputFilePath);
+
+            Position part1Position = new();
+            Position part2Position = new();
+
+            foreach (Instruction instruction in inputData)
+            {
+                part1Position.RunInstruction(instruction);
+                part2Position.RunWithAim(instruction);
+            }
+
+            Console.WriteLine($"Part One: {part1Position.horizontal * part1Position.depth}");
+            Console.WriteLine($"Part Two: {part2Position.horizontal * part2Position.depth}");
+        }
+
+    }
+
+}
